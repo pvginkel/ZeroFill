0345fa8f8158fd1227df75072664859255c1e201
Pieter van Ginkel <pvginkel@gmail.com>
2022-01-28 23:07:31 +0100

    Initial commit.

---
+++ ZeroFill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ZeroFill
+{
+    public static class Program
+    {
+        private const int BufferSize = 64 * 1024;
+
+        public static void Main()
+        {
+            using (var file = File.Create("zero.dat", BufferSize, FileOptions.DeleteOnClose | FileOptions.WriteThrough))
+            {
+                var buffer = new byte[BufferSize];
+
+                while (true)
+                {
+                    try
+                    {
+                        file.Write(buffer, 0, buffer.Length);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
